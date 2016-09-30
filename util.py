604ccd507ac7461b9092963ab92be31471a94037
Chris Hickey <chickey@Chriss-MacBook-Air.local>
2016-09-30 02:46:08 -0700

    debugging

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import hashlib, time, random, re, base64
 import customexception
 from ConfigParser import SafeConfigParser
 parser = SafeConfigParser()
-parser.read("./config")
+parser.read("/codename/config")
 
 class Util:
     @staticmethod
