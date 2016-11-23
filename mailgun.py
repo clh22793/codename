57a48add1deba7696bedf5f06ea458bad966bff8
Chris Hickey <chickey@Chriss-MacBook-Air.local>
2016-11-23 00:46:05 -0800

    minor updates

--- mailgun.py
+++ mailgun.py
@@ -1,11 +1,8 @@
 import customexception
-#import unirest
 import requests
 
 class Email:
-    #API_KEY = "key-681208735ecee9dcc1364b20a78ee769"
     API_KEY = "key-681208735ecee9dcc1364b20a78ee769"
-    #API_BASE_URL = "https://api.mailgun.net/v3/magicstack.io"
     API_BASE_URL = "https://api.mailgun.net/v3/kloudbit.com"
     DEV_EMAIL = "clh10242016@mailinator.com"
 
@@ -20,7 +17,7 @@ class Email:
         html += "<br><br>One quick question if you don't mind me asking...why did you sign up?"
         html += "<br><br>Please reply here and let me know.  I would love to hear more about what you're working on."
         html += "<br><br>Chris"
-        html += "<br><br>PS: Read our <a href='http://www.kloudbit.com/documentation'>documentation</a> or <a href='http://dashboard.kloudbit.com/'>create an API</a> to get started!"
+        html += "<br><br>PS: Read our <a href='http://www.kloudbit.com/documentation'>documentation</a> or <a href='http://dashboard.kloudbit.com/'>create an application</a> to get started!"
         html += "<br><br>--<br>Chris Hickey<br>Founder, <a href='http://www.kloudbit.com'>Kloudbit</a>"
 
         params = {"to":to, "bcc":"chris@kloudbit.com", "from":"Kloudbit Team <chris@kloudbit.com>", "subject": "Welcome to Kloudbit!", "html":html}
